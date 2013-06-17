8327b679e3964a2be09f8548c185a22e7bd632d5
Aleksej <deletesoftware@yandex.ru>
2013-06-17 13:25:30 +0400

    Show forecast for the current deck.

--- Calc_optgroup_ease.py
+++ Calc_optgroup_ease.py
@@ -5,6 +5,9 @@
 
 from anki.hooks import wrap
 from aqt.overview import Overview
+from Card_time_forecast import getForecast
+from anki.utils import fmtTimeSpan
+
 
 def getThisGroupsEase(mw):
     """Get the average ease for the decks in this options group."""
@@ -113,11 +116,37 @@ def makeEaseWarnerStrings(mw):
     return retval
 
 
+def makeForecastStrings(mw):
+
+    ids = mw.col.findCards('deck:current is:review -is:suspended')
+
+    if len(ids) > 5000:
+        return ''
+
+    years = 10
+    forecast_days = 365.25 * years
+
+    total = 0
+    for cid in ids:
+        f = getForecast(mw, mw.col.getCard(cid), forecast_days)
+        if f:
+            total += f
+
+    per_day = total / forecast_days
+    if per_day > 0:
+        per_day_text = u" ({0}/day)".format(fmtTimeSpan(per_day, short=True))
+    else:
+        per_day_text = ''
+    return u"Next {0}Y: {1}".format(years, fmtTimeSpan(total, short=True), per_day_text)
+
+
+
+
 def myTable(self, _old):
 
     oldRes = _old(self)
 
-    gr = makeEaseWarnerStrings(self.mw)
+    gr = makeEaseWarnerStrings(self.mw) + "<br />" + makeForecastStrings(self.mw)
     if gr == '':
         return oldRes
     else:
